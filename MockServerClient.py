bce2a179f69be331fbf7b7372cf5504103cabf84
53066191 <53066191@qq.com>
2019-02-22 16:16:09 +0800

    Create MockServerClient.py

---
+++ MockServerClient.py
@@ -0,0 +1,218 @@
+# encoding: utf-8
+"""
+@author: liuyun
+@time: 2019/1/11/011 18:29
+@desc:
+"""
+
+
+import collections
+import json
+import requests
+import threading
+
+from WebSocketClient import WebSocketClient
+
+
+class MockServerClient(object):
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.expectations = []
+        self.websockets = {}
+        self.threads = []
+
+
+    def _call(self, command, data=None):
+        base_url = "http://{}:{}".format(self.ip, self.port)
+        return requests.put("{}/{}".format(base_url, command), data=data)
+
+    def reset(self):
+        self.expectations = []
+        self._call("reset")
+
+    def mock_callback(self, requestMatcher, requestHandler):
+        ws = WebSocketClient(self.ip, self.port)
+        self.websockets[ws.clientId] = ws
+        ws.clientHander = requestMatcher
+        ws.requestHandler = requestHandler
+        t = threading.Thread(target=ws.run, args=())
+        t.start()
+
+    def callback_close(self, id):
+        self.websockets[id].on_close()
+
+
+    def stub(self, request, response, timing=None, time_to_live=None):
+        self._call("expectation", json.dumps(_non_null_options_to_dict(
+            _Option("httpRequest", request),
+            _Option("httpResponse", response),
+            _Option("times", (timing or _Timing()).for_expectation()),
+            _Option("timeToLive", time_to_live, formatter=_to_time_to_live)
+        )))
+
+    def expect(self, request, response, timing, time_to_live=None):
+        self.stub(request, response, timing, time_to_live)
+        self.expectations.append((request, timing))
+
+    def verify(self):
+        for req, timing in self.expectations:
+            result = self._call("verify", json.dumps({
+                "httpRequest": req,
+                "times": timing.for_verification()
+            }))
+            assert result.status_code == 202, result.content.decode('UTF-8').replace('\n', '\r\n')
+
+
+def request(method=None, path=None, querystring=None, body=None, headers=None, cookies=None):
+    return _non_null_options_to_dict(
+        _Option("method", method),
+        _Option("path", path),
+        _Option("queryStringParameters", querystring, formatter=_to_named_values_list),
+        _Option("body", body),
+        _Option("headers", headers, formatter=_to_named_values_list),
+        _Option("cookies", cookies)
+    )
+
+
+def request_for_callback(client_id, request, timing=None):
+    return _non_null_options_to_dict(
+        _Option("httpRequest", request),
+        _Option("times", (timing or _Timing()).for_expectation()),
+        _Option("httpResponseObjectCallback", client_id.getIdInfo())
+    )
+
+
+
+def response(code=None, body=None, headers=None, cookies=None, delay=None):
+    return _non_null_options_to_dict(
+        _Option("statusCode", code),
+        _Option("body", body),
+        _Option("headers", headers, formatter=_to_named_values_list),
+        _Option("delay", delay, formatter=_to_delay),
+        _Option("cookies", cookies)
+    )
+
+def clientId(id):
+    return ClientId(id)
+
+def times(count):
+    return _Timing(count)
+
+
+def form(form):
+    # NOTE(lindycoder): Support for mockservers version before https://github.com/jamesdbloom/mockserver/issues/371
+    return collections.OrderedDict((("type", "PARAMETERS"), ("parameters", _to_named_values_list(form))))
+
+
+def json_equals(payload):
+    """Expects that the request payload is equal to the given payload."""
+    return collections.OrderedDict((("type", "JSON"), ("json", json.dumps(payload)), ("matchType", "STRICT")))
+
+
+def json_contains(payload):
+    """Expects the request payload to match all given fields. The request may has more fields."""
+    return collections.OrderedDict(
+        (("type", "JSON"), ("json", json.dumps(payload)), ("matchType", "ONLY_MATCHING_FIELDS")))
+
+
+def json_response(body=None, headers=None, **kwargs):
+    headers = (headers or {})
+    headers["Content-Type"] = "application/json"
+    return response(body=json.dumps(body), headers=headers, **kwargs)
+
+
+class _Option:
+    def __init__(self, field, value, formatter=None):
+        self.field = field
+        self.value = value
+        self.formatter = formatter or (lambda e: e)
+
+
+class _Timing:
+    def __init__(self, count=None):
+        self.count = count
+
+    def for_expectation(self):
+        if self.count:
+            return {"remainingTimes": self.count, "unlimited": False}
+        else:
+            return {"unlimited": True}
+
+    def for_verification(self):
+        return {"exact": True, "count": self.count}
+
+
+class ClientId():
+
+    def __init__(self, clientId):
+        self.clientId = clientId
+
+    def getIdInfo(self):
+        return {"clientId": self.clientId}
+
+
+class _Time:
+    def __init__(self, unit, value):
+        self.unit = unit
+        self.value = value
+
+
+def seconds(value):
+    return _Time("SECONDS", value)
+
+
+def milliseconds(value):
+    return _Time("MILLISECONDS", value)
+
+
+def microseconds(value):
+    return _Time("MICROSECONDS", value)
+
+
+def nanoseconds(value):
+    return _Time("NANOSECONDS", value)
+
+
+def minutes(value):
+    return _Time("MINUTES", value)
+
+
+def hours(value):
+    return _Time("HOURS", value)
+
+
+def days(value):
+    return _Time("DAYS", value)
+
+
+def _non_null_options_to_dict(*options):
+    return {o.field: o.formatter(o.value) for o in options if o.value is not None}
+
+
+def _to_named_values_list(dictionary):
+    return [{"name": key, "values": [value]} for key, value in dictionary.items()]
+
+
+def _to_time(value):
+    if not isinstance(value, _Time):
+        value = seconds(value)
+    return value
+
+
+def _to_delay(delay):
+    delay = _to_time(delay)
+    return {
+        "timeUnit": delay.unit,
+        "value": delay.value
+    }
+
+
+def _to_time_to_live(time):
+    time = _to_time(time)
+    return {
+        "timeToLive": time.value,
+        "timeUnit": time.unit,
+        "unlimited": False
+    }
+
